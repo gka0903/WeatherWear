469cf8f1f31547f6d349b6308f6d771e96b6ad48
gka0903 <ham3252@naver.com>
2023-10-29 22:48:19 +0900

    geoCoding 하드코딩으로 구현

--- src/pages/GetLocation/destinationLocation/useGeoCoding.ts
+++ src/pages/GetLocation/destinationLocation/useGeoCoding.ts
@@ -2,15 +2,16 @@ import key from "../../../keys/geoCoding";
 
 const useGeoCoding = () => {
     const apiKey = key;
-    const address = '1600 Amphitheatre Parkway, Mountain View, CA';
+    const address = '한성대학교';
 
-    // const formattedAddress = encodeURIComponent(address);
+    const formattedAddress = encodeURIComponent(address);
 
-    fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=Toledo&key=${apiKey}`)
+    fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${formattedAddress}&key=${apiKey}`)
         .then(response => response.json())
         .then(data => {
             if (data.status === 'OK') {
                 const formattedResult = data.results[0].formatted_address;
+                console.log(data);
                 console.log(`Address for ${address}: ${formattedResult}`);
             } else {
                 console.error('Unable to find the address for the specified place.');
