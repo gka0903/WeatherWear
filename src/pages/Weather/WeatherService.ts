4fa584168051a1430596d5b0ce05e19aac992083
jaemin0211 <you@example.com>
2023-11-13 15:20:14 +0900

    test

--- src/pages/Weather/WeatherService.ts
+++ src/pages/Weather/WeatherService.ts
@@ -1,6 +1,7 @@
 // WeatherService.ts
+import KEY from "../../keys/env";
 
-const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = KEY;
 
 
 console.log(`API Key: ${API_KEY}`); // API 키 출력하여 확인
@@ -14,6 +15,7 @@ interface WeatherData {
 export async function getWeather(lat: number, lon: number): Promise<WeatherData | null> {
     try {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`);
+        //xml로 받고싶으면 metric뒤에 &mode=xml 추가작성, 근데 xml로하려면 밑에도 수정필요, 밑에는 JSON임
         if (!response.ok) {
             throw new Error('Weather data fetch failed');
         }
