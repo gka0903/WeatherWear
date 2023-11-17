528c28cb7857461e3b49e9f6f1d6438eec370dd0
song-yup <songsy0107@gmail.com>
2023-11-17 22:18:49 +0900

    clothingdetail

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -14,6 +14,8 @@ function Home() {
         <AppLayout>
             <Section>
                 <Location/>
+            </Section>
+            <Section>
                 <RecommendClothing/>
             </Section>
             <Section>
