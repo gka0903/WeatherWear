e0360331f3ac793cc09eaef9f8f02bcc7e767e6a
gka0903 <ham3252@naver.com>
2023-11-18 18:03:40 +0900

    clothesSlice

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import Home from './pages/Home'
+import {store} from "./pages/redux/store";
+import {Provider} from "react-redux";
 
 function App() {
-  return <Home />;
+    return (
+        <Provider store={store}>
+            <Home/>
+        </Provider>
+    );
 }
 
 export default App;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,7 @@ import AppLayout from "../../components/AppLayout";
 
 import Location from "../GetLocation/destinationLocation/Location";
 
-import {Provider} from "react-redux";
+import {useSelector} from "react-redux";
 
 import React from "react";
 import {Route, Routes} from "react-router-dom";
@@ -11,25 +11,26 @@ import {Route, Routes} from "react-router-dom";
 import Wear from "../Weather/Wear";
 import FortuneCookie from "../FortuneCookie/FortuneCookie";
 import NotFound from "../notFound/notFound";
-import {store} from "../redux/store";
+import {RootState} from "../redux/store";
 import RecommendClothing from "../Clothing/RecommendClothing";
+import ClothingDetails from "../Clothing/ClothingDetails";
 
 
 function Home() {
+    const currentTemp = useSelector((state: RootState) => state.temperature.value);
+    const currentClothing = useSelector((state: RootState) => state.clothes.value);
     return (
-        <Provider store={store}>
-            <AppLayout>
-                <Routes>
-                    <Route path="/" element={<Section><Location/></Section>}></Route>
-                    <Route path="/wear" element={<Section><Wear/></Section>}></Route>
-                    <Route path="/details" element={<Section><RecommendClothing/></Section>}/>
-                    {/*<Route path="/Details/:id"*/}
-                    {/*       element={<ClothingDetails currentTemp={currentTemp} currentClothing={currentClothing}/>}/>*/}
-                    <Route path="/fortune" element={<Section><FortuneCookie/></Section>}></Route>
-                    <Route path="*" element={<NotFound/>}></Route>
-                </Routes>
-            </AppLayout>
-        </Provider>
+        <AppLayout>
+            <Routes>
+                <Route path="/" element={<Section><Location/></Section>}></Route>
+                <Route path="/wear" element={<Section><Wear/></Section>}></Route>
+                <Route path="/details" element={<Section><RecommendClothing/></Section>}/>
+                <Route path="/details/:id"
+                       element={<ClothingDetails currentTemp={currentTemp} currentClothing={currentClothing}/>}/>
+                <Route path="/fortune" element={<Section><FortuneCookie/></Section>}></Route>
+                <Route path="*" element={<NotFound/>}></Route>
+            </Routes>
+        </AppLayout>
     );
 }
 
