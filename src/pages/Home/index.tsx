2c38395a9c23825e16cdfc46cbe2b71fbff96f9d
gka0903 <ham3252@naver.com>
2023-11-19 11:03:27 +0900

    err fix

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,20 +9,18 @@ import {store} from "../redux/store";
 import React from "react";
 import {Route, Routes} from "react-router-dom";
 
-import Wear from "../Weather/Wear";
 import FortuneCookie from "../FortuneCookie/FortuneCookie";
 import NotFound from "../notFound/notFound";
 
 
 function Home() {
-    const geo = useGeolocation(); 
+    const geo = useGeolocation();
     console.log(geo);
     return (
         <Provider store={store}>
             <AppLayout>
                 <Routes>
                     <Route path="/" element={<Section><Location/></Section>}></Route>
-                    <Route path="/wear" element={<Section><Wear/></Section>}></Route>
                     <Route path="/fortune" element={<Section><FortuneCookie/></Section>}></Route>
                     <Route path="*" element={<NotFound/>}></Route>
                 </Routes>
--- src/pages/Weather/WeatherService.ts
+++ src/pages/Weather/WeatherService.ts
@@ -1,40 +1,36 @@
-// WeatherService.ts
-import KEY from "../../keys/env";
-
-
 import KEY from "../../keys/env";
 
 const API_KEY = KEY;
 
 interface WeatherForecast {
-  time: string;
-  temp: number;
-  icon: string;
+    time: string;
+    temp: number;
+    icon: string;
 }
 
 export async function getWeatherForecast(lat: number, lon: number): Promise<WeatherForecast[] | null> {
-  try {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`);
-    if (!response.ok) {
-      throw new Error('Weather forecast fetch failed');
-    }
-    const data = await response.json();
-    
-    const currentTime = new Date();
-    const endTime = new Date(currentTime.getTime() + 24 * 60 * 60 * 1000);
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`);
+        if (!response.ok) {
+            throw new Error('Weather forecast fetch failed');
+        }
+        const data = await response.json();
 
-    const forecasts = data.list.filter((item: any) => {
-      const forecastTime = new Date(item.dt_txt);
-      return forecastTime >= currentTime && forecastTime <= endTime;
-    }).map((item: any) => ({
-      time: item.dt_txt,
-      temp: item.main.temp,
-      icon: item.weather[0].icon,
-    }));
-    
-    return forecasts;
-  } catch (error) {
-    console.error('Error fetching weather forecast data:', error);
-    return null;
-  }
+        const currentTime = new Date();
+        const endTime = new Date(currentTime.getTime() + 24 * 60 * 60 * 1000);
+
+        const forecasts = data.list.filter((item: any) => {
+            const forecastTime = new Date(item.dt_txt);
+            return forecastTime >= currentTime && forecastTime <= endTime;
+        }).map((item: any) => ({
+            time: item.dt_txt,
+            temp: item.main.temp,
+            icon: item.weather[0].icon,
+        }));
+
+        return forecasts;
+    } catch (error) {
+        console.error('Error fetching weather forecast data:', error);
+        return null;
+    }
 }
